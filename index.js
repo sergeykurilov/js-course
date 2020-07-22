aa19f17c1f9c39c341b91012bd0a836b1c827d50
Sergey <kurilovsergey15@gmail.com>
2020-07-22 22:23:39 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+let incr = 10,
+    decr = 10;
+  
+  // // ++incr;
+  // // --decr;
+
+  // console.log(incr++);
+  // console.log(--decr);
+
+  // console.log(5%2);
+
+  // console.log(2*4 === '8');
+
+  // const isChecked = true,
+  //       isClose = false;
+
+// console.log(isChecked || !isClose);
+
+console.log(2+2*2 !== '6');
