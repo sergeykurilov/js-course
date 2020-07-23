cc5f5313ad6a5a4cfdc75948e4818c2eecb2b43a
Sergey <kurilovsergey15@gmail.com>
2020-07-23 23:14:05 +0300

    Используем функции 18

--- script.js
+++ script.js
@@ -83,14 +83,7 @@
 
 // *************** Решение
 
-const numberOfFilms = +prompt("Сколько фильмов просмотрел пользователь");
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
-}
+
 // let i = 0;
 // while(i < 2){
 //     i++;
@@ -105,7 +98,7 @@ const personalMovieDB = {
 //     }
 // }
 
-let i = 0;
+// let i = 0;
 
 // do{
 //     i++;
@@ -121,22 +114,13 @@ let i = 0;
 // }while(i < 2);
 
 
-// for(let i = 0; i < 2; i++){
-//     const a = prompt('Один из последних просмотренных фильмов?'),
-//           b = prompt('На сколько оцените его?');
-//     if(a !== null && b != null && a != '' && b != '' && a.length < 50){
-//         personalMovieDB.movies[a] = b;
-//         console.log('done')
-//     }else{
-//         console.log('error');
-//         i--;
-//     }
+
           
 
 // } 
 
 
-console.log(personalMovieDB)
+
 
 
 //   while (!a || a.length > 50) {
@@ -145,13 +129,91 @@ console.log(personalMovieDB)
 
 //       } 
 
-// if(personalMovieDB.count < 10){
-//    alert("Просмотрено довольно мало фильмов");
-// }else if(personalMovieDB.count >10 && personalMovieDB.count < 30){
-//    alert("Вы классический зритель");
-// }else if(personalMovieDB.count > 30){
-//    alert("Вы киноман");
+
+
+
+
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+ };
+// function start(){
+//      numberOfFilms = +prompt('Сколько фильмов просмотрел пользователь', '');
+
+
+//      while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+//         numberOfFilms = +prompt('Сколько фильмов просмотрел пользователь', '');
+//      }
 // }
-// else{
-//    alert("Произошла ошибка");
+
+// start();
+
+
+// function rememberMyFilms(){
+//     for(let i = 0; i < 2; i++){
+//         const a = prompt('Один из последних просмотренных фильмов?'),
+//               b = prompt('На сколько оцените его?');
+//         if(a !== null && b != null && a != '' && b != '' && a.length < 50){
+//             personalMovieDB.movies[a] = b;
+//             console.log('done')
+//         }else{
+//             console.log('error');
+//             i--;
+//         }    
 // }
+// }
+
+// rememberMyFilms();
+
+// function detectPersonalLevel(){
+//     if(personalMovieDB.count < 10){
+//     alert("Просмотрено довольно мало фильмов");
+//  }else if(personalMovieDB.count >10 && personalMovieDB.count < 30){
+//     alert("Вы классический зритель");
+//  }else if(personalMovieDB.count > 30){
+//     alert("Вы киноман");
+//  }
+//  else{
+//     alert("Произошла ошибка");
+//  }
+// }
+
+// detectPersonalLevel();
+
+
+function showMyDB(hidden){
+    if(!hidden){
+        console.log(personalMovieDB)
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        const a = prompt(`Ваш любимый жанр под номером ${i}`); 
+        personalMovieDB.genres.push(a)
+}
+}
+
+console.log(writeYourGenres())
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
---
+++ script2.js
@@ -0,0 +1,70 @@
+'use strict';
+// let num = 20;
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 10;
+//     console.log(num)
+// }
+
+// showFirstMessage('Hey Man, whats up?');
+
+// console.log(num);
+
+// // function calc(a,b){
+// //     return a + b;
+// // }
+
+// function ret(){
+//     let num = 50;
+//     return num;
+// }
+
+
+
+
+
+// const anotherNumber = ret()
+// console.log(anotherNumber)
+
+
+
+
+
+// const logger = function(){
+//     console.log('Hey')
+// };
+// logger()
+
+// const calc = (a,b) => a + b;
+
+// console.log(calc(222,222))
+
+// ********************************************
+
+const str = "tesuhuhuhuWWWWWWWWt";
+const arr = [1,2,4]
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"))
+
+const logg = 'Hey man';
+
+console.log(logg.slice(0))
+
+console.log(logg.substring(1,2))
+console.log(logg.substr(1, 3))
+
+
+const num = 12.2;
+
+console.log(Math.round(num))
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+
+console.log(parseFloat(test))
+
+
+
