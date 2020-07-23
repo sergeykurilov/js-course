5a50854387c00ad088e7b62ba92f10c7f5efdf4f
Sergey <kurilovsergey15@gmail.com>
2020-07-23 20:46:44 +0300

    Расписал цикл

--- script.js
+++ script.js
@@ -91,23 +91,49 @@ const personalMovieDB = {
    genres: [],
    privat: false
 }
+// let i = 0;
+// while(i < 2){
+//     i++;
+//         const a = prompt('Один из последних просмотренных фильмов?'),
+//           b = prompt('На сколько оцените его?');
+//     if(a !== null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//     }else{
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+let i = 0;
+
+// do{
+//     i++;
+//         const a = prompt('Один из последних просмотренных фильмов?'),
+//           b = prompt('На сколько оцените его?');
+//     if(a !== null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//     }else{
+//         console.log('error');
+//         i--;
+//     }
+// }while(i < 2);
 
 
-
-
-for(let i = 0; i < 2; i++){
-    const a = prompt('Один из последних просмотренных фильмов?'),
-          b = prompt('На сколько оцените его?');
-    if(a !== null && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log('done')
-    }else{
-        console.log('error');
-        i--;
-    }
+// for(let i = 0; i < 2; i++){
+//     const a = prompt('Один из последних просмотренных фильмов?'),
+//           b = prompt('На сколько оцените его?');
+//     if(a !== null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//     }else{
+//         console.log('error');
+//         i--;
+//     }
           
 
-} 
+// } 
 
 
 console.log(personalMovieDB)
