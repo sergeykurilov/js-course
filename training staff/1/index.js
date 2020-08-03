9eae374121af1392f1c16c0d23530b6fc733e753
Sergey <kurilovsergey15@gmail.com>
2020-08-03 23:21:34 +0300

    Burger menu Practic

--- training staff/1/index.js
+++ training staff/1/index.js
@@ -1,4 +1,4 @@
-let menu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.mobile-menu');
 const menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', function(){
@@ -6,4 +6,29 @@ menuButton.addEventListener('click', function(){
 
 });
 
+// ***********************
 
+const tab = document.querySelector('.tab'),
+      tabLinks = tab.querySelectorAll('.tab-links'),
+      tabContent = document.querySelectorAll('.tab-content');
+
+function removeContent(){
+    for(let eachContentBlock of tabContent){
+        eachContentBlock.classList.add('-hide');
+        eachContentBlock.classList.remove('-active');
+    }
+}
+
+function addContent(){
+    for(let eachContentBlock of tabContent){
+        for(let menuItem of tabLinks){
+            menuItem.addEventListener('click', ()=>{
+                eachContentBlock.classList.remove('-hide');
+                eachContentBlock.classList.add('-active');
+            });
+        }
+    }
+}
+addContent();
+
+removeContent();
