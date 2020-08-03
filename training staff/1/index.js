b1cd13e7ac72626eec4a7487e2abe58289ba6404
Sergey <kurilovsergey15@gmail.com>
2020-08-03 23:02:58 +0300

    Burger menu Practic

---
+++ training staff/1/index.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('.mobile-menu');
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', function(){
+    menu.classList.toggle('-active');
+
+});
+
+
