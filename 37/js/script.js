ae62a2d1453a7442a3d66f1a66ef19fffde07f2f
Sergey <kurilovsergey15@gmail.com>
2020-08-05 03:55:12 +0300

    Cards Classes

--- 37/js/script.js
+++ 37/js/script.js
@@ -160,7 +160,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
-    const modalTimerId = setTimeout(openModal, 3000);
+    // const modalTimerId = setTimeout(openModal, 3000);
 
     function showModalByScroll() {
         window.addEventListener('scroll', ()=>{
@@ -173,4 +173,106 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+
+
+    // Используем классы для карточек
+
+//     class postCard {
+//         constructor(imgPath, itemSubtitle, itemDescr, menuItemTotal){
+//             this.imgPath = imgPath;
+//             this.itemSubtitle = itemSubtitle;
+//             this.itemDescr = itemDescr;
+//             this.menuItemTotal = menuItemTotal;
+//         }
+    
+//         cardAdd(){
+//             return this.imgPath , Subtitle = this.itemSubtitle, Descr = this.itemDescr, Price = this.menuItemTotal;
+//          }
+//     }
+
+
+
+// let cardFirst = new postCard('./img/slider/food-12.jpg','Меню "Фитнес', 'Description', 123);
+// console.log(cardFirst);
+
+// let menuItemBlock = document.querySelectorAll('.menu__item'),
+//       Image = document.querySelector('img'),
+//       Subtitle = document.querySelector('.menu__item-subtitle'),
+//       Descr = document.querySelector('.menu__item-descr'),
+//       Price = document.querySelector('.menu__item-total>span');
+
+// console.log(+Price.textContent);
+
+    class MenuCard {
+        constructor(src,alt, title, descr, price, parentSelector){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH(){
+            this.price = +this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+             </div>
+            </div>
+            `;
+
+            this.parent.append(element);
+        }
+    }
+
+
+    // const div = new MenuCard()
+    // div.render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        '"Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        '"Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        '"Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+
+
+ 
 });
