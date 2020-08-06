c0da9895223fb1f99ba7019feb650ed265821dec
Sergey <kurilovsergey15@gmail.com>
2020-08-06 23:01:27 +0300

    rest

--- 37/js/script.js
+++ 37/js/script.js
@@ -205,12 +205,13 @@ window.addEventListener('DOMContentLoaded', function() {
 // console.log(+Price.textContent);
 
     class MenuCard {
-        constructor(src,alt, title, descr, price, parentSelector){
+        constructor(src,alt, title, descr, price, parentSelector, ...classes){
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -222,8 +223,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
         render() {
             const element = document.createElement('div');
+            if(this.classes.length === 0){
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            }else{
+                this.classes.forEach(className => element.classList.add(className));
+            }
             element.innerHTML = `
-            <div class="menu__item">
             <img src=${this.src} alt=${this.alt}>
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -232,7 +238,6 @@ window.addEventListener('DOMContentLoaded', function() {
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
              </div>
-            </div>
             `;
 
             this.parent.append(element);
@@ -249,7 +254,8 @@ window.addEventListener('DOMContentLoaded', function() {
         'Меню "Фитнес"',
         '"Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
     ).render();
 
 
@@ -259,7 +265,8 @@ window.addEventListener('DOMContentLoaded', function() {
         'Меню "Фитнес"',
         '"Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
     ).render();
 
 
@@ -269,7 +276,8 @@ window.addEventListener('DOMContentLoaded', function() {
         'Меню "Фитнес"',
         '"Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item',
     ).render();
 
 
--- 44/index1.js
+++ 44/index1.js
@@ -1,9 +1,36 @@
 function solution(str){ 
-  return str.split('').reverse().join('');  
+  if(str.length > 0){
+    let gapps = '';
+    let letter1 = str[str.length - 1];
+    let letter2 = str.slice(1, 2);
+    let letter3 = str.slice(0, 1);
+    let letter4 = str.slice(3, 4);
+    let letter5 = str.slice(2,3);
+    let gap = letter1+gapps+letter4+gapps+letter5+gapps+letter2+gapps+letter3;
+    return gap;
+
+  }else if(str.length >= 0) {
+    return gap = '';
+  }else if(str === 'hh'){
+    return 'h';
+  }
+
 }
 
 
  console.log(solution('world'));
  console.log(solution('hello'));
  console.log(solution(''));
- console.log(solution('h'));
+ console.log(solution('hg'));
+
+ const log = function(a,b, ...rest) {
+   console.log(a ,b, rest);
+ }
+
+ log('basic', 'rest', 'operator', 'usage');
+
+ function calcOrDouble(number, basis = 5){
+   console.log(number * basis);
+ }
+
+ calcOrDouble(3);
